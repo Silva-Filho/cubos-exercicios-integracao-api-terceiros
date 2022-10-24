30f3f543e47ac6d5862afeafec2aacb1a118a7be
Silva Filho <tarusilva@gmail.com>
2022-10-24 20:24:15 -0300

    refactor: adiciona o middleware verifyDomain ao router

--- classe-01/src/router.js
+++ classe-01/src/router.js
@@ -1,10 +1,18 @@
 const express = require( "express" );
 
+// Middlewares
+const { verifyDomain } = require( "./middlewares/infosCompany" );
+
+// Controllers
 const { getInfosEmpresas } = require( "./controllers/infosCompanys" );
 
 const router = express();
 
-router.get( "/empresas/:dominioEmpresa", getInfosEmpresas );
+router.get(
+    "/empresas/:dominioEmpresa",
+    verifyDomain,
+    getInfosEmpresas
+);
 
 module.exports = {
     router
