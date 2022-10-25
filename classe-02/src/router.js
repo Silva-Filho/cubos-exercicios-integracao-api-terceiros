df6762135493dfa4883b9faf932602a712dabce3
Silva Filho <tarusilva@gmail.com>
2022-10-24 21:01:08 -0300

    refactor: adiciona os middlewares a rota POST
    Adiciona os middlewares verifyIp e verifyVote a
    rota POST votacao/:pais/:ip.

--- classe-02/src/router.js
+++ classe-02/src/router.js
@@ -1,10 +1,19 @@
 const express = require( "express" );
 
+// Middlewares
+const { verifyIp, verifyVote } = require( "./middlewares/election" );
+
+// Controllers
 const { saveVote, getAllVotes } = require( "./controllers/election" );
 
 const router = express();
 
-router.post( "/votacao/:pais/:ip", saveVote );
+router.post(
+    "/votacao/:pais/:ip", 
+    verifyIp, 
+    verifyVote, 
+    saveVote
+);
 router.get( "/votacao", getAllVotes );
 
 module.exports = {
